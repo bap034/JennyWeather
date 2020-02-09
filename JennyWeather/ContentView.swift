3961c10200541be097b6d2d35e25fa7fa07de082
Brett Petersen <bap034@gmail.com>
2020-02-09 14:23:51 -0800

    created xcode project

---
+++ JennyWeather/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  JennyWeather
+//
+//  Created by Brett Petersen on 2/9/20.
+//  Copyright © 2020 Brett Petersen. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ JennyWeatherTests/JennyWeatherTests.swift
@@ -0,0 +1,34 @@
+//
+//  JennyWeatherTests.swift
+//  JennyWeatherTests
+//
+//  Created by Brett Petersen on 2/9/20.
+//  Copyright © 2020 Brett Petersen. All rights reserved.
+//
+
+import XCTest
+@testable import JennyWeather
+
+class JennyWeatherTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
